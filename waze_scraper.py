ee76a9f8e26d6f5a6036795ce02b8ddf01078648
fredderf1234 <fredmarb@hotmail.fr>
2025-07-12 00:02:22 +0200

    Create waze_scraper.py

---
+++ waze_scraper.py
@@ -0,0 +1,13 @@
+def get_waze_alertes():
+    return [
+        {
+            "type": "police",
+            "lat": 48.8566,
+            "lon": 2.3522,
+            "description": "Contrôle",
+            "distance": 5.2,
+            "time_since": "3 min",
+            "validations": 12,
+            "new": True
+        }
+    ]
